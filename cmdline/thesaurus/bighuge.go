e349ea07d750057bed6fe09b02cd06a2eeaeb03e
kodmm <53547701+kodmm@users.noreply.github.com>
2021-07-02 02:12:48 +0900

    remove os.Stdout fmt,

--- cmdline/thesaurus/bighuge.go
+++ cmdline/thesaurus/bighuge.go
@@ -3,7 +3,6 @@ package thesaurus
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
@@ -24,8 +23,6 @@ func (b *BigHuge) Synonyms(term string) ([]string, error) {
 	var syns []string
 	response, err := http.Get("http://words.bighugelabs.com/api/2/" +
 		b.APIKey + "/" + term + "/json")
-	hoge, err := ioutil.ReadAll(response.Body)
-	fmt.Println(hoge)
 	if err != nil {
 		return syns, fmt.Errorf("bighuge: %qの類語検索に失敗しました: %v", term, err)
 	}
