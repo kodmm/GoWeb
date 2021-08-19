d53acb0cd2f22b96e772057516a4bbd2d39a1468
kodmm <53547701+kodmm@users.noreply.github.com>
2021-08-19 20:55:32 +0900

    Twitterからの読み込み

--- socialpoll/twittervotes/twitter.go
+++ socialpoll/twittervotes/twitter.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
+	"log"
 	"net"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
 	"time"
+
+	"github.com/garyburd/go-oauth/oauth"
+	"github.com/garyburd/go-outh/oauth"
+	"github.com/joeshaw/envdecode"
+	"github.com/joho/godotenv"
 )
 
 var conn net.Conn
@@ -31,4 +42,106 @@ func closerConn() {
 	if reader != nil {
 		reader.Close()
 	}
+}
+
+var(
+	authClient *oauth.Client
+	httpClient *http.Client
+)
+func setupTwitterAuth() {
+
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var ts struct {
+		ConsumerKey string `env:"SP_TWITTER_KEY,required"`
+		ConsumerSecret string `env:"SP_TWITTER_SECRET, required"`
+		AccessToken string `env:"SP_TWITTER_ACCESSTOKEN, required"`
+		AccessSecret string `env:"SP_TWITTER_ACCESSSECRET, required"`
+	}
+	if err := envdecode.Decode(&ts); err != nil {
+		log.Fatalln(err)
+	}
+	creds = &oauth.Credentials{
+		Token: ts.AccessToken,
+		Secret: ts.AccessSecret,
+	}
+	authClient = &outh.Client{
+		Credentials: oauth.Credentials{
+			Token: ts.ConsumerKey,
+			Secret: ts.ConsumerSecret,
+		},
+	}
+}
+
+
+var (
+	authSetupOnce sync.Once
+	httpClient *http.Client
+)
+
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(
+		func() {
+			setupTwitterAuth()
+			httpClient = &http.Client{
+				Transport: &http.Transport{
+					Dial: dial,
+				},
+			}
+		}
+	)
+	formEnc := params.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+	req.Header.Set("Authorization",
+		authClient.AuthorizationHeader(creds, "POST", req.URL, params)
+	)
+	return httpClient.Do(req)
+}
+
+type tweet struct {
+	Text string
+}
+
+func readFromTwitter(votes chan<- string) {
+	options, err := loadOptions()
+	if err != nil {
+		log.Println("選択肢の読み込みに失敗しました:", err)
+		return 
+	}
+	u, err := url.Parse("https://stream.twitter.com/1.1/statuses/filter.json")
+	if err != nil {
+		log.Println("URLの解析に失敗しました。:", err)
+		return
+	}
+	query := make(url.Values)
+	query.Set("track", strings.Join(options, ","))
+	req, err := http.NewRequest("POST", u.String(), strings.NewReader(query.Encode()))
+	if err != nil {
+		log.Println("検索のリクエストの作成に失敗しました。:", err)
+		return 
+	}
+	resp, err := makeRequest(req, query)
+	if err != nil {
+		log.Println("検索のリクエストに失敗しました。")
+		return 
+	}
+	reader = resp.Body
+	decoder := json.NewDecoder(reader)
+
+	for {
+		var tweet tweet
+		if err := decoder.Decode(&tweet); err != nil {
+			break
+		}
+		for _, option := range options {
+			if strings.Contains(strings.ToLower(tweet.Text), strings.ToLower(option)) {
+				log.Println("投票:", option)
+				votes <- option
+			}
+		}
+	}
+
 }
