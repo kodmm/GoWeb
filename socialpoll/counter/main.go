12071e3081d7721c083a319b800de021aaea7e47
kodmm <53547701+kodmm@users.noreply.github.com>
2021-10-28 04:13:16 +0900

    データベースへの接続

---
+++ socialpoll/counter/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	mgo "gopkg.in/mgo.v2"
+)
+
+var fatalErr error
+
+func fatal(e error) {
+	fmt.Println(e)
+	flag.PrintDefaults()
+	fatalErr = e
+}
+
+func main() {
+	defer func() {
+		if fatalErr != nil {
+			os.Exit(1)
+		}
+	}()
+	log.Println("データベースに接続します...")
+	db, err := mgo.Dial("localhost")
+	if err != nil {
+		fatal(err)
+		return
+	}
+	defer func() {
+		log.Println("データベース接続を閉じます...")
+		db.Close()
+	}()
+
+}
