987422e1599ec0c9fe205193b2e37a11eff5a09c
kodmm <53547701+kodmm@users.noreply.github.com>
2021-11-01 18:08:43 +0900

    ハンドラ間でのデータの共有

---
+++ socialpoll/api/vars.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"sync"
+)
+
+var (
+	varsLock sync.RWMutex
+	vars     map[*http.Request]map[string]interface{}
+)
+
+func OpenVars(r *http.Request) {
+	varsLock.Lock()
+	if vars == nil {
+		vars = map[*http.Request]map[string]interface{}{}
+	}
+	vars[r] = map[string]interface{}{}
+	varsLock.Unlock()
+}
+
+func CloseVars(r *http.Request) {
+	varsLock.Lock()
+	delete(vars, r)
+	varsLock.Unlock()
+}
