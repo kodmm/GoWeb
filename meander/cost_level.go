f0a1b89b06be38c9c0f78107c99c4ece0f290828
kodmm <53547701+kodmm@users.noreply.github.com>
2021-11-28 09:02:44 +0900

    テスト駆動型の開発

--- meander/cost_level.go
+++ meander/cost_level.go
@@ -1,6 +1,12 @@
 package meander
 
+import "strings"
+
 type Cost int8
+type CostRange struct {
+	From Cost
+	To   Cost
+}
 
 const (
 	_ Cost = iota
@@ -10,3 +16,35 @@ const (
 	Cost4
 	Cost5
 )
+
+var costStrings = map[string]Cost{
+	"$":     Cost1,
+	"$$":    Cost2,
+	"$$$":   Cost3,
+	"$$$$":  Cost4,
+	"$$$$$": Cost5,
+}
+
+func (l Cost) String() string {
+	for s, v := range costStrings {
+		if l == v {
+			return s
+		}
+	}
+	return "不正な値です"
+}
+
+func ParseCost(s string) Cost {
+	return costStrings[s]
+}
+
+func (r CostRange) String() string {
+	return r.From.String() + "..." + r.To.String()
+}
+func ParseCostRange(s string) *CostRange {
+	segs := strings.Split(s, "...")
+	return &CostRange{
+		From: ParseCost(segs[0]),
+		To:   ParseCost(segs[1]),
+	}
+}
