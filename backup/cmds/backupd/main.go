0bc3193eccabf408a67e4fcdd4307bc94ea7e37b
kodmm <53547701+kodmm@users.noreply.github.com>
2022-04-24 17:25:19 +0900

    データのキャッシュ

---
+++ backup/cmds/backupd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/kodmm/GoWeb/backup"
+	"github.com/matryer/filedb"
+)
+
+type path struct {
+	Path string
+	Hash string
+}
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			log.Fatalln(fatalErr)
+		}
+	}()
+	var (
+		interval = flag.Int("interval", 10, "チェックの間隔(秒単位)")
+		archive  = flag.String("archive", "archive", "アーカイブの保存先")
+		dbpath   = flag.String("db", "./db", "filedbデータベースへのパス")
+	)
+	flag.Parse()
+
+	m := &backup.Monitor{
+		Destination: *archive,
+		Archiver:    backup.ZIP,
+		Paths:       make(map[string]string),
+	}
+	db, err := filedb.Dial(*dbpath)
+	if err != nil {
+		fatalErr = err
+		return
+	}
+	defer db.Close()
+	col, err := db.C("paths")
+	if err != nil {
+		fatalErr = err
+		return
+	}
+
+}
